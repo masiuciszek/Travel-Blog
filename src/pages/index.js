c0055178d2cc3cb6288204676369ccce3f24b8cc
Masiu <ciszekmarcell@gmail.com>
2019-11-10 20:16:35 +0100

    gatsby image and chaged the hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,52 @@
 /* eslint-disable react/display-name */
 import React from 'react';
 import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import SimpleHero from '../components/SimpleHero';
 import Banner from '../components/Banner';
 import { BtnShadow } from '../components/styled/Buttons';
 import About from '../components/home/About';
 import Services from '../components/home/Services';
+import StyledHero from '../components/StyledHero';
 
-export default () => (
-  <>
-    <Layout>
-      <SimpleHero>
-        <Banner
-          title="Exploring the nature"
-          info="traveling around the world and exploring the real you!"
-        >
-          <AniLink fade to="/tours">
-            <BtnShadow>Explore Tours</BtnShadow>
-          </AniLink>
-        </Banner>
-      </SimpleHero>
-      <About />
-      <Services />
-    </Layout>
-  </>
-);
+const Home = ({ data }) => {
+  console.log(data);
+  return (
+    <>
+      <Layout>
+        <StyledHero img={data.heroBg.childImageSharp.fluid} home="true">
+          <Banner
+            title="Exploring the nature"
+            info="traveling around the world and exploring the real you!"
+          >
+            <AniLink fade to="/tours">
+              <BtnShadow>Explore Tours</BtnShadow>
+            </AniLink>
+          </Banner>
+        </StyledHero>
+        <About />
+        <Services />
+      </Layout>
+    </>
+  );
+};
+
+export const HOME_BACKGROUND_IMAGE_QUERY = graphql`
+  query {
+    heroBg: file(relativePath: { eq: "hero.jpg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
+
+Home.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Home;
