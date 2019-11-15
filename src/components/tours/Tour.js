a024d5cb7161de2ff657948293f3397bc5ee239e
Masiu <ciszekmarcell@gmail.com>
2019-11-15 20:25:36 +0100

    add

--- src/components/tours/Tour.js
+++ src/components/tours/Tour.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Image from 'gatsby-image';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
-const StyledTour = styled.section`
+const StyledTour = styled.article`
   padding: 1rem;
+  box-shadow: ${props => props.theme.lightShadow};
+  transition: ${props => props.theme.mainTransition};
+  .img-wrapper {
+    position: relative;
+    /* background: ${props => props.theme.primaryColor}; */
+    transition: ${props => props.theme.mainTransition};
+    /* box-shadow: ${props => props.theme.lightShadow}; */
+    padding: 0.2rem;
+    img {
+      transition: ${props => props.theme.mainTransition};
+      box-shadow: ${props => props.theme.lightShadow};
+
+    }
+  }
 `;
 
 const Tour = ({ tour }) => {
-  console.log(tour, ' from tour component');
+  const { name, price, slug, country, images } = tour;
+
   return (
     <StyledTour>
       <h1>Tour</h1>
+      <div className="img-wrapper">
+        <Image fluid={images[0].fluid} />
+      </div>
     </StyledTour>
   );
 };
